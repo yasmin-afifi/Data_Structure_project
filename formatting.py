109425f0cbc84ca8618104df096a4b619ae7f21f
yasmin afifi <67705548+yasmin-afifi@users.noreply.github.com>
2021-12-17 16:49:07 +0200

    final update adding prettifying function

--- formatting.py
+++ formatting.py
@@ -27,3 +27,54 @@ def adding_spaces(ind):
         string += "     "
         i += 1
     return string
+
+#prettifying the xml
+def formatting(text):
+    
+    xml = removing_spaces(text)
+    ind =0
+    new_xml = str()
+    
+    i =0
+ 
+    
+    while i < len(xml):
+        
+        if i+1  < len(xml) and xml[i+1] == "<":
+            if i+2 < len(xml) and xml[i+2] != "/":
+            
+                ind+=1
+                new_xml += xml[i]
+                
+                i+=1
+            
+                if (xml[i-1 ] == ">"):
+                
+                    new_xml += "\n"
+                    new_xml += adding_spaces(ind)
+                
+                
+                
+            else:
+            
+                new_xml += xml[i]
+                
+                i+=1
+            
+                if (xml[i-1 ] == ">"):
+                
+                    new_xml += "\n"
+                    new_xml += adding_spaces(ind)
+                         
+                ind -=1 
+            
+            
+        if i + 1 < len(xml):
+            if ( xml[i] == '?' or xml[i] == '/') and xml[i + 1] == '>':
+                ind -= 1
+        
+        new_xml += xml[i]
+        i += 1            
+            
+    return new_xml
+
